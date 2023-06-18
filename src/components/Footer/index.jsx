adf095eef951ed085a17c1153a64e71f81bba9c3
shohsultoncode <shohsulton.3039@gmail.com>
2023-06-18 20:23:44 +0500

    .

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -59,7 +59,6 @@ const Footer = () => {
                         <label htmlFor="email_subscribe" className="footer-heading">Subscribe</label>
                         <div className="form-group">
                            <input type="text" className="form-control py-4" id="email_subscribe" placeholder="Email" />
-                           <input type="button" className="btn btn-sm btn-primary" />
                            {/* ///easdfghjkssss? */}
                         </div>
                      </form>
